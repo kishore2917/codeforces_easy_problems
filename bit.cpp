568e3bd9e65d12cdd3dcdf0f162ca730bb6afcd1
Kishore Kumar <123622196+kishore2917@users.noreply.github.com>
2026-09-03 21:27:25 +0530

    282A
    Bit ++

---
+++ bit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    int x=0;
+    while(n--){
+        cin>>s;
+        if(s[0]=='+' || s[1]=='+') x++;
+        else x--;
+        
+    }
+    cout<<x;
+}
